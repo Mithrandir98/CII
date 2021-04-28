81faec755a2bfcf788961f4f93be579401f839f7
Ruthwik <aspire1998@gmail.com>
2021-04-28 14:27:26 +0700

    Initialize bootstrap

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
 
+http
+  .createServer(function (req, res) {
+    fs.readFile("home.html", function (err, data) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(3000);
